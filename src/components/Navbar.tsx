c7dd9e8d1cc26a584822b20ec841391617adffa9
Anton <108577345+makushenkoao@users.noreply.github.com>
2023-03-07 15:35:19 +0200

    add burger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import {AppBar, Box, CssBaseline, Toolbar, Typography} from "@mui/material";
-import {Link, NavLink} from "react-router-dom";
+import React, {useState} from 'react';
+import {AppBar, Drawer, IconButton, Toolbar} from "@mui/material";
+import { Link, NavLink } from "react-router-dom";
+import {ChevronLeft, Menu} from '@mui/icons-material'
+
+
+const drawerWidth = 240;
 
 export const Navbar = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
     return (
         <AppBar color='inherit' position='fixed' >
-            <Toolbar style={{display: 'flex', justifyContent: 'space-between'}}>
+            <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <IconButton
+                    onClick={() => setIsOpen(true)}
+                >
+                    <Menu fontSize={'large'}/>
+                </IconButton>
                 <div>
                     <Link
                         to='/'
@@ -13,11 +23,27 @@ export const Navbar = () => {
                     >
                         Rick & Morty<span style={{color: 'orange', marginLeft: '6px'}}>Wiki</span></Link>
                 </div>
-                <div style={{display: 'flex', gap: '10px', fontSize: '20px'}}>
-                    <NavLink to='/'>Characters</NavLink>
-                    <NavLink to='/episode'>Episode</NavLink>
-                    <NavLink to='/location'>Location</NavLink>
-                </div>
+                <Drawer
+                    open={isOpen}
+                    sx={{
+                        width: drawerWidth,
+                        flexShrink: 0,
+                        '& .MuiDrawer-paper': {
+                            width: drawerWidth,
+                        },
+                    }}
+                >
+                    <IconButton
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <ChevronLeft fontSize={'large'}/>
+                    </IconButton>
+                    <div style={{display: 'flex', flexDirection: 'column', gap: '10px', fontSize: '20px', padding: '0 15px', marginTop: '15px'}}>
+                        <NavLink to='/'>Characters</NavLink>
+                        <NavLink to='/episode'>Episode</NavLink>
+                        <NavLink to='/location'>Location</NavLink>
+                    </div>
+                </Drawer>
             </Toolbar>
         </AppBar>
     );
